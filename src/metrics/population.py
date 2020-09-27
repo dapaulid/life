8e1edc0ab9f5c19829c5c3a92fca08f4ca7ff450
Daniel Pauli <dapaulid@gmail.com>
2020-09-27 14:08:37 +0200

    Add population metric.

--- src/metrics/population.py
+++ src/metrics/population.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 """
@@ -14,41 +13,28 @@
 # imports
 #-------------------------------------------------------------------------------
 #
-from world import World
-from plot import Plot
-from rule import Rule
-from rule_gen import RuleGenerator
-from compressor import Compressor
-
-# metrics we want to apply to our world
-from metrics.cyclic import Cyclic
-from metrics.exploration import Exploration
-from metrics.compressibility import Compressibility
-from metrics.population import Population
+from metric import Metric
 
 #-------------------------------------------------------------------------------
-# main
+# class definition
 #-------------------------------------------------------------------------------
 #
+class Population(Metric):
 
-# create some random rule
-geni = RuleGenerator(2)
-rule = geni.random()
-# avoid "flicker-worlds"
-rule.array[0] = 0
-rule.save("last_random")
+	#---------------------------------------------------------------------------
+    ## constructor
+    def __init__(self, world):
+        # call base
+        Metric.__init__(self, world)
+        self.name = "population"
+        self.value = self.world.get_population()
+	# end function
 
-#rule = Rule.load("hourglass")
+    def update(self):
+        self.value = self.world.get_population()
+    # end function
 
-# create a world governed by this rule
-world = World(rule, 
-    radius=50, 
-    metrics=[Exploration, Compressibility, Population, Cyclic]
-)
-
-# display the world over time
-plot = Plot()
-plot.show(world)
+# end class
 
 #-------------------------------------------------------------------------------
 # end of file
