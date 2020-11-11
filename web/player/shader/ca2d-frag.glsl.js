daebb515b5f367690070acac0fdf930357a6409f
Daniel Pauli <dapaulid@gmail.com>
2020-11-11 21:10:53 +0100

    Towards labels.

--- web/player/shader/ca2d-frag.glsl.js
+++ web/player/shader/ca2d-frag.glsl.js
@@ -22,6 +22,12 @@ precision mediump float;
 
 // returns the logical state of a cell pixel
 #define CELL_STATE(cell)      int(0.5 + cell.g)
+// returns the label (color index) of a cell pixel
+#define CELL_LABEL(cell)      int(0.5 + mod(cell.b, float(MAX_LABELS)))
+
+// return a vec4 from an array represented as "pseudo" 1D texture
+#define LOOKUP(u2d, i, n)     texture2D(u2d, vec2((float(i) + 0.5) / float(n), 0.5))
+
 // returns the new cell color
 #define RETURN(color)         gl_FragColor = color; return;
 
@@ -39,16 +45,18 @@ uniform sampler2D   u_world;
 uniform vec2        u_worldSize;
 uniform sampler2D   u_rule;
 uniform highp int   u_ruleSize;
+uniform sampler2D   u_labels;
 uniform lowp  int   u_states;
 uniform bool        u_tick;
 uniform bool        u_fade;
-uniform vec2        u_neighCoord [NEIGH_SIZE] ;
+uniform vec2        u_neighCoord [NEIGH_SIZE];
 
 //------------------------------------------------------------------------------
 // constants
 //------------------------------------------------------------------------------
 //
-vec4 error        = vec4(0.5, 0.0, 0.5, 1.0); // purple
+const vec4 error        = vec4(0.5, 0.0, 0.5, 1.0); // purple
+
 
 //------------------------------------------------------------------------------
 // main
@@ -62,45 +70,56 @@ void main() {
 		RETURN(cell);
 	}
 
-	lowp int oldState = CELL_STATE(cell);
-	//vec2 px = vec2(1.0, 1.0)/u_worldSize;
-
-	/*
-	int labelCount [MAX_LABELS];
-	for (int j = 0; j < MAX_LABELS; j++) {
-		if (j == idx) {
-			labelCount[j]++;
-		}
-	}*/
-
+	// index into rule array
 	highp int idx = 0;
+	// array for determining the most frequent label
+	lowp int labelCount [MAX_LABELS];
+
+	// iterate over cells in neighborhood (including center)
 	for (int i = NEIGH_SIZE - 1; i >= 0; i--) {
-		idx = idx*u_states + CELL_STATE(texture2D(u_world, v_cellCoord + u_neighCoord[i]));
-	}
+		vec4 neigh = texture2D(u_world, v_cellCoord + u_neighCoord[i]);
+		
+		// add state to rule index
+		idx = idx*u_states + CELL_STATE(neigh);
 
-	/*
-		determine index into rule array using convolution with exponential weights:
+		// increment count for label
+		lowp int label = CELL_LABEL(neigh);
+		for (int l = 0; l < MAX_LABELS; l++) {
+			if (l == label) {
+				labelCount[l]++;
+			} // end if
+		} // end for
+	} // end for
 
-			 4 | 3 | 2
-			---+---+---
-			 5 | 0 | 1
-			---+---+---
-			 6 | 7 | 8
-
-		hint: read the following statements backwards for correct order
-	*/
+	// determine most frequent label (mode)
+	lowp int modeLabel = 0;
+	lowp int modeCount = 0; // to select label 0 only if no others
+	for (int l = 1; l < MAX_LABELS; l++) {
+		if (labelCount[l] > modeCount) {
+			modeLabel = l;
+			modeCount = labelCount[l];
+		} // end if
+	} // end for
 
 #if DEBUG
-	// sanity check
-	if (idx < 0 || idx > 511) {
+	// sanity checks
+	if ((idx < 0) || (idx >= 512)) {
+		RETURN(error);
+	}
+	if ((modeLabel < 0) || (modeLabel >= MAX_LABELS)) {
 		RETURN(error);
 	}
 #endif
 
-	vec4 newCell = texture2D(u_rule, vec2((float(idx) + 0.5) / float(u_ruleSize), 0.5));
+	// determine new cell
+	vec4 newCell = LOOKUP(u_rule, idx, u_ruleSize);
+
+	// determine label for new cell
+	vec4 labelColor = LOOKUP(u_labels, modeLabel, MAX_LABELS);
 
 	// handle fading
 	if (u_fade) {
+		lowp int oldState = CELL_STATE(cell);
 		lowp int newState = CELL_STATE(newCell);
 		if (newState == 0) {
 			// handle fade out
